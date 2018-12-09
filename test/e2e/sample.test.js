6f7420af572b5b91193217076dc1de5df76bf299
Kelly Dwan <ryelle@users.noreply.github.com>
2018-12-09 16:10:33 -0600

    Format the violations before checking length

--- test/e2e/sample.test.js
+++ test/e2e/sample.test.js
@@ -1,10 +1,12 @@
 const { loadPage } = require( 'axe-puppeteer' );
 const { WP_BASE_URL } = require( './support/config' );
+const formatAxeResults = require( './support/format-axe-results' );
 
 describe( 'a11y', () => {
 	it( 'runs sample test', async () => {
 		const axeBuilder = await loadPage( browser, WP_BASE_URL );
 		const results = await axeBuilder.analyze();
-		expect( results.violations.length ).toBe( 0 );
+		const formattedResults = formatAxeResults( results );
+		expect( formattedResults ).toHaveLength( 0 );
 	} );
 } );
