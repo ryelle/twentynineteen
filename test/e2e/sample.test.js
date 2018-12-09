6987b698881b7f1fcbc8659368d0d26b1d129304
Kelly Dwan <ryelle@users.noreply.github.com>
2018-12-09 16:14:36 -0600

    Update example tests

--- test/e2e/sample.test.js
+++ test/e2e/sample.test.js
@@ -2,10 +2,30 @@ const { loadPage } = require( 'axe-puppeteer' );
 const { WP_BASE_URL } = require( './support/config' );
 const formatAxeResults = require( './support/format-axe-results' );
 
-describe( 'a11y', () => {
-	it( 'runs sample test', async () => {
+describe( 'theme accessibility', () => {
+	it( 'header should have no violations', async () => {
 		const axeBuilder = await loadPage( browser, WP_BASE_URL );
+		// Only test a section of the page.
+		axeBuilder.include( '.site-header' );
 		const results = await axeBuilder.analyze();
+		const formattedResults = formatAxeResults( results );
+		expect( formattedResults ).toHaveLength( 0 );
+	} );
+
+	it( 'site content should have no violations', async () => {
+		const axeBuilder = await loadPage( browser, WP_BASE_URL );
+		// Only test a section of the page.
+		axeBuilder.include( '.site-main' );
+		const results = await axeBuilder.analyze();
+		const formattedResults = formatAxeResults( results );
+		expect( formattedResults ).toHaveLength( 0 );
+	} );
+
+	it( 'a single page should have no violations', async () => {
+		const axeBuilder = await loadPage( browser, `${WP_BASE_URL}/?page_id=2` );
+		const results = await axeBuilder.analyze();
+		// Excluding site-header, checked earlier.
+		axeBuilder.exclude( '.site-header' );
 		const formattedResults = formatAxeResults( results );
 		expect( formattedResults ).toHaveLength( 0 );
 	} );
